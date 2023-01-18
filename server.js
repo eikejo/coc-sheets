67d02a7c9b6a0d642895784fa442f49f08ed9ae4
Eike J <dev.eike.jo@gmail.com>
2023-01-18 22:48:40 +0100

    added connection to db + initial models

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const { logger, logEvent } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
+
+const connectDB = require("./config/dbConnection");
+const mongoose = require("mongoose");
+
+connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-// receive and parse json data, this one is based on body-parser, so no need to install bp
-app.use(express.json())
+// --- middleware ---
+
+app.use(logger);
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 
 // dirname is a global variable looking inside the current folder
 // This is useful for static files f.e. css files or 404 pages
@@ -14,17 +29,33 @@ app.use("/", require("./routes/root"));
 
 // Catch all route in case of errors
 app.all("*", (req, res) => {
-    res.status(404)
-    // Depending on who's or what's asking we're sending back a different kind of 404 message 
-    if (req.accepts("html")) {
-        res.sendFile(path.join(__dirname, "views", "404.html"))
-    } else if (req.accepts("json")) {
-        res.json({"message": "404 not found"})
-    } else {
-        res.type("txt").send("404 not found")
-    }
-})
+  res.status(404);
+  // Depending on who's or what's asking we're sending back a different kind of 404 message
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
+app.use(errorHandler);
+
+// Only listen if we've got a db connection
+mongoose.connection.once("open", () => {
+  console.log("Connected to mongoDB");
+  app.listen(PORT, () => {
+    console.log(
+      `Server is listening on ${PORT} in ${process.env.NODE_ENV} mode`
+    );
+  });
+});
+
+mongoose.connection.once("error", (err) => {
+  console.log(err);
+  logEvent(
+    `${err.no}\t${err.code}\t${err.syscall}\t${err.hostname}`,
+    "dbError-" + new Date().toISOString().split("T")[0] + ".log"
+  );
 });
