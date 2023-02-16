eec215c8165c272050d381c22a23f5eef1138787
Eike J <dev.eike.jo@gmail.com>
2023-02-16 20:25:11 +0100

    fixed sessions model

--- backend/models/Session.js
+++ backend/models/Session.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const sessionSchema = new mongoose.Schema({
-  characters: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  players: [{type:mongoose.Schema.Types.ObjectId, ref: "User"}],
+  characters: [{ type: mongoose.Schema.Types.ObjectId, ref: "Sheet" }],
   keeper: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   active: { // Allow Admins to disable users, in case of a convention or something
     type: Boolean,
